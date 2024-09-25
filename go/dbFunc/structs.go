95cc3a8fd009124e0dda4040c6522cccc4a9ae7f
flisa <lisa.fedoul>
2024-09-25 13:51:05 +0200

    la base de données

---
+++ go/dbFunc/structs.go
@@ -0,0 +1,26 @@
+package dbFunc
+
+type Experience struct {
+	ID         int
+	Nom        string
+	Entreprise string
+	Poste      string
+	DateDebut  string
+	DateFin    string
+}
+
+type Formation struct {
+	ID            int
+	Nom           string
+	Formation     string
+	Etablissement string
+	DateDebut     string
+	DateFin       string
+}
+
+type Projet struct {
+	ID        int
+	NomProjet string
+	LienRepo  string
+	Langage   string
+}
