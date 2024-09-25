5f762044885a8fab72cc360f53d36bf255d3803b
Itstritix <jiabao.tsui@gmail.com>
2024-09-25 11:27:54 +0200

    update main.go, add indexHandler.go

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"portfolio/go/handlers"
+)
 
+type webserver struct {
+	core *http.ServeMux
+	port int
+}
+
+func main() {
+	server := webserver{
+		core: http.NewServeMux(),
+		port: 8080,
+	}
+
+	server.Router()
+	server.Launch()
+}
+
+func (s *webserver) Router() {
+	s.core.HandleFunc("/index", handlers.IndexHandler)
+
+	s.core.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./src/css"))))
+	s.core.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("./src/img"))))
+	s.core.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./src/js"))))
+	fmt.Printf("http://localhost:%d \n", s.port)
+}
+
+func (s *webserver) Launch() {
+	// Lancement du serveur HTTP sur le port spécifié
+	http.ListenAndServe(fmt.Sprintf(":%d", s.port), s.core)
 }
