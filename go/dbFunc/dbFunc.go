37d278032a9e11e89c4d1b449b2efff72841ea23
flisa <lisa.fedoul>
2024-09-26 09:41:41 +0200

    fonction permettant de charger la base de données

--- go/dbFunc/dbFunc.go
+++ go/dbFunc/dbFunc.go
@@ -1,1 +1,22 @@
 package dbFunc
+
+import (
+	"database/sql"
+	"log"
+)
+
+var dbname = "./assets/db/forum.db"
+
+type DBPortfolio struct {
+	core *sql.DB
+}
+
+//var DB = LoadDb()
+
+func LoadDb() DBPortfolio {
+	dbIn, err := sql.Open("sqlite3", dbname)
+	if err != nil {
+		log.Fatal("Erreur lors de l'ouverture de la base de données:", err)
+	}
+	return DBPortfolio{core: dbIn}
+}
