c97b8b25cbcab1a0233593e654cb211bfcfa2656
flisa <lisa.fedoul>
2024-09-27 10:46:26 +0200

    rajout de la fonctions get utilisateur depuis la bd

---
+++ go/dbFunc/dbutilisateur.go
@@ -0,0 +1,19 @@
+package dbFunc
+
+import (
+	"database/sql"
+)
+
+// Fonction pour récupérer un utilisateur avec son nom d'utilisateur
+func GetUtilisateur(db DBPortfolio, nomUtilisateur string) (*Utilisateur, error) {
+	var utilisateur Utilisateur
+	query := "SELECT id, nom_utilisateur, mot_de_passe FROM utilisateur WHERE nom_utilisateur = ?"
+	err := db.core.QueryRow(query, nomUtilisateur).Scan(&utilisateur.ID, &utilisateur.NomUtilisateur, &utilisateur.MotDePasse)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &utilisateur, nil
+}
